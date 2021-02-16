ac1a25046d981f50215beadbcd1ac9574599c4e4
Emily Gann <aspentree79@gmail.com>
2021-02-16 14:06:45 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super();
+        // must call super method to invoke constructor of parent class. Including this prevents error message saying state cannot be defined. It is now defined through inclusion of super().
+        this.state = { displayBio: false };
+
+        console.log('Component this', this);
+
+        this.toggleDisplayBio = this.toggleDisplayBio.bind(this);
+    }
+
+    toggleDisplayBio() {
+        this.setState({displayBio: !this.state.displayBio});
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello!</h1>
+                <p>My name is Emily. I'm a software engineer.</p>
+                <p>I'm always looking forward to learning new things.</p>
+                {
+                this.state.displayBio ? (
+                    <div>
+                        <p>I live in the D.C. area and enjoy coding every day.</p>
+                        <p>My favorite languages are JavaScript and Ruby.</p>
+                        <p>When not coding, I enjoy discovering new music and playing with my three dogs.</p>
+                        <button onClick={this.toggleDisplayBio}>Show less</button>
+                    </div>
+                ) : (
+                    <div>
+                        <button onClick={this.toggleDisplayBio}>Read more</button>
+                    </div>
+                    )
+                }
+            </div>
+        )
+    }
 }
 
 export default App;
